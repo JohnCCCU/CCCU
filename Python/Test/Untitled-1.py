db21dd7d585f84ec739e1d8e65e095144363ce23
JohnCCCU <jbraunsdorf@calcoastcu.org>
2026-04-08 09:29:32 -0700

    created a test function

---
+++ Python/Test/Untitled-1.py
@@ -0,0 +1,17 @@
+# -*- Code with UTF-8 -*-
+"""
+Scope: testing py code with in VSCode
+
+Created on 04/08/2026 09:28:00
+
+Created by John Braunsdorf
+
+"""
+
+x = []
+def append_func(x):
+   for i in range(10):
+      x.append(i)
+   return x
+
+y = append_func(x)
